579e28fd3995a4d9275ea98bf9cfdccca8c92956
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2023-12-30 19:27:47 -0600

    Add: A. Modify the studentData table to include personal information as the last item

---
+++ class-roster-project/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+
+/**
+ Course Title: C867 - SCRIPTING AND PROGRAMMING - APPLICATIONS
+ TOOLS: Cpp
+ IDE: VISUAL STUDIO
+ STUDENT ID: 010195113
+ AUTHOR: NICK MENSAH
+*/
+
+// A.  Modify the “studentData Table” to include your personal information as the last item.
+
+const std::string studentData[] = {
+    "A1,John,Smith,John1989@gmail.com,20,30,35,40,SECURITY",
+    "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+    "A3,Jack,Napoli,The_lawyer99@yahoo.com,19,20,40,33,SOFTWARE",
+    "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+    "A5,Nicholas,Mensah,nmensa2@wqu.edu,39,30, 60, 90,SOFTWARE"
+};
+
+ // Class_Roster
